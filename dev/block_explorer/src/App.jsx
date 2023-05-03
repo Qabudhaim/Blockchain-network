9c71f1acdced487f34027cdef6b81f44a0ffc806
Qusai <Qusai.Abudhaim@gmail.com>
2023-05-03 07:49:03 +0200

    Add block explorer footprint

---
+++ dev/block_explorer/src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState, useRef } from 'react'
+
+function App() {
+
+  const submitButtonRef = useRef(null);
+
+  const [data, setData] = useState([])
+  const [selectedOption, setSelectedOption] = useState('users');
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [inputValue, setInputValue] = useState('1');
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      submitButtonRef.current.click();
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setButtonClicked(true);
+  };
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${selectedOption}/${inputValue}`)
+    .then(response => response.json())
+    .then(json => setData(json))
+    .then(setButtonClicked(false))
+    }, [buttonClicked])
+
+
+  return (
+<>
+      <h1>Explorer</h1>
+      <div>
+        <input onChange={handleInputChange} onKeyDown={handleKeyPress}></input>
+        <button ref={submitButtonRef} onClick={handleButtonClick}>Filter</button>
+      </div>
+
+      <div>
+        <form>
+          <div>
+            <label>
+              <input type="radio" value="users" checked={selectedOption === 'users'} onChange={handleOptionChange} />
+              users
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="radio" value="posts" checked={selectedOption === 'posts'} onChange={handleOptionChange} />
+              posts
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="radio" value="comments" checked={selectedOption === 'comments'} onChange={handleOptionChange} />
+              comments
+            </label>
+          </div>
+        </form>
+        <p>You selected: {selectedOption}</p>
+      </div>
+
+      <div>      
+        { JSON.stringify(data) === '{}' ? <p>No Data available</p> : <p>{JSON.stringify(data)}</p> }
+      </div>
+    </>
+
+  )
+}
+
+export default App
